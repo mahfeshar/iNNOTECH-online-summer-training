0ac6b5c3d0f2bb59863f200d07033306b99e7ab7
mahfeshar <mahmoudfeshar11@gmail.com>
2024-07-13 16:22:19 +0300

    Solving the task 2 and 3 with photos to more details

---
+++ Tasks 1/Task  2 - 3/PrintNumbers/Program.cs
@@ -0,0 +1,89 @@
+﻿
+using System.Diagnostics;
+using System.Text;
+
+string? input;
+long maximumNumber;
+
+do
+{
+    Console.Write("Please enter the maximum number you need to print: ");
+    input = Console.ReadLine();
+    if (long.TryParse(input, out maximumNumber))
+    {
+        if (maximumNumber > 0)
+            break;
+        else
+            Console.WriteLine("Number cannot be negative or zero, Please enter valid number.");
+    }
+    else
+        Console.WriteLine("Invalid input. Enter a valid integer");
+}
+while (true);
+
+Console.WriteLine("------------------------------------------------");
+// Using Normal String
+
+Console.WriteLine("Using Normal String");
+
+string resultNumbers;
+Stopwatch stopwatch;
+
+stopwatch = Stopwatch.StartNew();
+resultNumbers = string.Empty;
+
+for (int i = 1; i <= maximumNumber; i++)
+{
+    // We need to remove comma from the end
+    // First Sol
+
+    if (i == 1)
+        resultNumbers += i.ToString();
+    else
+        resultNumbers += "," + i.ToString();
+
+    // Second sol
+    // We can make it from the end, when i == max -> print just number
+
+    // We can also try:
+    // if (i != 1)
+    //      result += ","
+    // result += i.ToString()
+
+    // But last one will increase the time (Bad Approach)
+}
+stopwatch.Stop();
+
+//Console.WriteLine($"All numbers from 1 to {maximumNumber} = {resultNumbers}");
+//Console.WriteLine();
+Console.WriteLine($"The time is {stopwatch.ElapsedMilliseconds} ms");
+
+stopwatch.Reset();
+
+
+Console.WriteLine("------------------------------------------------");
+// Using StringBuilder
+
+Console.WriteLine("Using StringBuilder");
+
+stopwatch = Stopwatch.StartNew();
+StringBuilder resultNumberWithBuilder;
+
+resultNumberWithBuilder = new StringBuilder();
+
+for (int i = 1; i <= maximumNumber; i++)
+{
+    if (i == 1)
+        resultNumberWithBuilder.Append(i.ToString());
+    else
+        resultNumberWithBuilder.Append($",{i.ToString()}");
+}
+
+stopwatch.Stop();
+
+//Console.WriteLine($"All numbers from 1 to {maximumNumber} = {resultNumberWithBuilder}");
+//Console.WriteLine();
+Console.WriteLine($"The time is {stopwatch.ElapsedMilliseconds} ms");
+
+stopwatch.Reset();
+Console.WriteLine("------------------------------------------------");
---
+++ Tasks 1/Task  2 - 3/SummingNumbers/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Diagnostics;
+
+
+string? input;
+long maximumNumber;
+
+do
+{
+    Console.Write("Please enter the maximum number: ");
+    input = Console.ReadLine();
+
+    if (long.TryParse(input, out maximumNumber))
+    {
+        if (maximumNumber > 0)
+        {
+            Console.WriteLine($"Are you sure you need to get the addition up to {maximumNumber}? (Y or y for yes, N or n for no)");
+            string? answer;
+            answer = Console.ReadLine();
+            if (answer == "Y" || answer == "y")
+                break;
+            else if (answer == "N" || answer == "n")
+                continue;
+            //TODO: Validate (answer)
+        }
+        else
+          Console.WriteLine("Number cannot be negative or zero, Please enter valid number.");
+    }
+    else
+        Console.WriteLine("Invalid input. Enter a valid integer");
+}
+while (true);
+
+
+Console.WriteLine("------------------------------------------------");
+// For Loop Approach
+Console.WriteLine("The ((FOR Loop Approach))");
+
+long sum;
+
+sum = 0;
+Stopwatch stopwatch;
+
+stopwatch = Stopwatch.StartNew();
+for (int i = 0; i <= maximumNumber; i++)
+{
+    sum += i;
+}
+stopwatch.Stop();
+Console.WriteLine($"The Sum = {sum} and It takes {stopwatch.ElapsedMilliseconds} ms");
+
+stopwatch.Reset();
+
+Console.WriteLine("------------------------------------------------");
+
+// Formula Approach 
+Console.WriteLine("The ((Formula Approach))");
+stopwatch.Start();
+sum = maximumNumber * (maximumNumber + 1) / 2;
+//Console.WriteLine($"Stop watch now = {stopwatch.ElapsedMilliseconds} ms");
+
+stopwatch.Stop();
+Console.WriteLine($"The Sum = {sum} and It takes {stopwatch.ElapsedMilliseconds} ms");
+
+stopwatch.Reset();
+
+Console.WriteLine("------------------------------------------------");
